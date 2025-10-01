d6f0294d24ea5b831297c5eeb4c88d66cd6de208
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 00:06:45 +0530

    03_int_char_and_float.c

--- 01_Basics/03_int_char_and_float.c
+++ 01_Basics/03_int_char_and_float.c
@@ -2,9 +2,16 @@
 #include<conio.h>
 void main()
 {
+//Integer 
 int i=5;
-float f=6;
-char c=A;
-printf("");
+printf("Integer is %d\n",i);
+
+//Float
+float f=6.2;
+printf("Float is %f\n",f);
+
+//Character 
+char c='A';
+printf("Character is %c",c);
 getch();
 }
