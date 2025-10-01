434b5caf5a0f03b2521565bc53c49f06e1392330
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-01 23:43:22 +0530

    02_Display_Name_in_Different_Way.c

--- 01_Basics/02.c
+++ 01_Basics/02.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
 void main()
-{
+{
+//Display Name at Centre of Star.
 printf("****************\n");
 printf("*              *\n");
 printf("* Abhishek Raj *\n");
 printf("*              *\n");
 printf("****************");
+
+//Display Name in Unique Style. 
+printf("My\n Name\n is\n Abhishek\n Raj");
 getch();
 }
