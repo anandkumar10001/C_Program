6f2871429c0d44209ba571782e7d171d18043f4e
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 00:47:15 +0530

    Create 04_rectangle.c

---
+++ 01_Basics/04_rectangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=4, b=5, c=6, area, volume;
+printf("Length of the rectangle is %d ,breath of the rectangle is %d and height of the rectangle is %d",a,b,c);
+
+//Area
+area=a*b;
+printf("Area of the rectangle is %d\n",area);
+
+\\Volume 
+volume=a*b*c;
+printf("Volume of the rectangle is %d",volume);
+getch();
+}
