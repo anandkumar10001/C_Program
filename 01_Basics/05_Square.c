f0cd467963956c55c554631e3b0cf4613d52e5fd
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 00:42:22 +0530

    Update 05_Square.c

--- 01_Basics/05_Square.c
+++ 01_Basics/05_Square.c
@@ -3,15 +3,15 @@
 void main()
 {
 int a=5, area, volume;
-printf("Side of square is %d\n",a);
+printf("Side of the square is %d\n",a);
 
 //Area
 area=a*a;
-printf("Area of Square is %d\n",area);
+printf("Area of the Square is %d\n",area);
 
 //Volume 
 volume=a*a*a;
-printf("Volume of Square is %d",volume);
+printf("Volume of the Square is %d",volume);
 
 getch();
 }
