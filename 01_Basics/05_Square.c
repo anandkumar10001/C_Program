34e0d0bb4166e81b1fe4e0feb09baa8468d723f0
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 00:38:47 +0530

    Create 05_Square.c

---
+++ 01_Basics/05_Square.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=5, area, volume;
+printf("Side of square is %d\n",a);
+
+//Area
+area=a*a;
+printf("Area of Square is %d\n",area);
+
+\\Volume 
+volume=a*a*a;
+printf("Volume of Square is %d",volume);
+
+getch();
+}
