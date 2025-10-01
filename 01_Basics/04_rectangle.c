cd815413ed6b06a2b1e19ecba005cfa1a242b0d8
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 00:48:22 +0530

    Update 04_rectangle.c

--- 01_Basics/04_rectangle.c
+++ 01_Basics/04_rectangle.c
@@ -3,13 +3,13 @@
 void main()
 {
 int a=4, b=5, c=6, area, volume;
-printf("Length of the rectangle is %d ,breath of the rectangle is %d and height of the rectangle is %d",a,b,c);
+printf("Length of the rectangle is %d ,breath of the rectangle is %d and height of the rectangle is %d\n",a,b,c);
 
 //Area
 area=a*b;
 printf("Area of the rectangle is %d\n",area);
 
-\\Volume 
+//Volume 
 volume=a*b*c;
 printf("Volume of the rectangle is %d",volume);
 getch();
