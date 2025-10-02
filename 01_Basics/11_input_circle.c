3d14bd3de408fe16710f363d5a079e83e791c9db
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 10:37:39 +0530

    Create 11_input_circle.c

---
+++ 01_Basics/11_input_circle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a=1.33, pi=3.14, r, area, volume;
+printf("Enter the value of radius of the circle ");
+
+//Area
+area=pi*r*r;
+printf("Area of the circle is %f\n",area);
+
+//Volume 
+volume=a*pi*r*r*r;
+printf("Volume of the sphere is %f",volume);
+getch();
+}
