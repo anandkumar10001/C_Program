fdb981201375dfee2bb2817c8c2a75fc720123e6
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 10:57:21 +0530

    Create 13_input_swaping.c

---
+++ 01_Basics/13_input_swaping.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x=, y=, z;
+printf("Enter the value for x );
+scanf("%d",&x);
+printf("Enter the value for y );
+scanf("%d",&y);
+printf("Value after swaping of x =%d and y =%d",x,y);
+z=x;
+x=y;
+y=z;
+printf("Value after swaping of x =%d and y =%d",x,y);
+getch();
+}
