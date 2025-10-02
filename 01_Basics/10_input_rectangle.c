f18b07917ab7f364e7523da1de8b0fce1f4967fe
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 10:29:04 +0530

    Create 10_input_rectangle.c

---
+++ 01_Basics/10_input_rectangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a, b, c, area, volume;
+printf("Enter the value of length of the rectangle ");
+scanf("%d",&a);
+printf("Enter the value of breath of the rectangle ");
+scanf("%d",&a);
+printf("Enter the value of height of the rectangle ");
+scanf("%d",&a);
+
+//Area
+area=a*b;
+printf("Area of the rectangle is %d\n",area);
+
+//Volume 
+volume=a*b*c;
+printf("Volume of the rectangle is %d",volume);
+getch();
+}
