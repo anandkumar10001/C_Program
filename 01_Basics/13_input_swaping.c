4cc4f3f1659aeb5561f8ba64e62d2f6616380b35
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 11:00:20 +0530

    Update 13_input_swaping.c

--- 01_Basics/13_input_swaping.c
+++ 01_Basics/13_input_swaping.c
@@ -2,12 +2,12 @@
 #include<conio.h>
 void main()
 {
-int x=, y=, z;
-printf("Enter the value for x );
+int x, y, z;
+printf("Enter the value for x ");
 scanf("%d",&x);
-printf("Enter the value for y );
+printf("Enter the value for y ");
 scanf("%d",&y);
-printf("Value after swaping of x =%d and y =%d",x,y);
+printf("Value after swaping of x =%d and y =%d\n",x,y);
 z=x;
 x=y;
 y=z;
