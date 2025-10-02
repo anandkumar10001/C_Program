9a281c48f7fb9f95458bbe0c4e56a5e14217b240
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 13:16:41 +0530

    Update 01.c

--- 01_Basics/01.c
+++ 01_Basics/01.c
@@ -6,6 +6,7 @@ printf("Name - Abhishek Raj\n");
 printf("Class - BCA\n");
 printf("Section - G\n");
 printf("Roll No - \n");
+printf("Semester - First\n");
 printf("Subject - Problem Solving Techniques\n");
 printf("Submitted to Ajay Sir");
 getch();
