c1166a73fe2b2f2bf2b39f34c8db63a5a0145784
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 11:09:32 +0530

    Update 01.c

--- 01_Basics/01.c
+++ 01_Basics/01.c
@@ -2,6 +2,11 @@
 #include<conio.h>
 void main()
 {
-printf("Abhishek Raj");
+printf("Name - Abhishek Raj\n");
+printf("Class - BCA\n");
+printf("Section - G\n");
+printf("Roll No - \n");
+printf("Subject - Problem Solving Techniques\n");
+printf("Submitted to Ajay Thakur");
 getch();
 }
