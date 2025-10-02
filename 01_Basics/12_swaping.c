c82a01ca92cdfb629c02b9b216c6d2194c7455b6
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-02 14:44:47 +0530

    Update 12_swaping.c

--- 01_Basics/12_swaping.c
+++ 01_Basics/12_swaping.c
@@ -4,10 +4,10 @@ void main()
 {
 int x=3, y=4, z;
 
-printf("Value before swaping of x =%d, y =%d and z =%d\n",x,y,z);
+printf("Value before swaping of x =%d and y =%d",x,y);
 z=x;
 x=y;
 y=z;
-printf("Value after swaping of x =%d, y =%d and z =%d",x,y,z);
+printf("Value after swaping of x =%d and y =%d",x,y);
 getch();
 }
