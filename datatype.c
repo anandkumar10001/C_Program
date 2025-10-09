dba4adb67ad8d05b45405e2ba64f11150d2df1d0
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-09 23:10:21 +0530

    Update datatype.c

--- datatype.c
+++ datatype.c
@@ -5,14 +5,20 @@ void main()
 //Primary (Basic) Data Types
 //integer 
 int age = 18;
-printf("Age : %d",age);
+printf("Age : %d\n",age);
 
 //float
 float flo = 2;
 float at = 12.34567;
-printf("%f",flo);
-printf("%.3f",at);
+printf("Value of float : %f\n",flo);
+printf("Value of float : %.3f\n",at);
+
 //double
+double pi = 3;
+double pii = 3.141592653589793;
+printf("Value of pi: %lf\n", pi); 
+printf("Value of pi: %.4lf\n", pii);
+
 //char
 //void
 
