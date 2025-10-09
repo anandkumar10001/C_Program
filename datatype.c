c9d155ba379ad31737704a4ea426b39a1734aef8
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-09 19:52:26 +0530

    Update datatype.c

--- datatype.c
+++ datatype.c
@@ -3,6 +3,24 @@
 void main()
 {
 //Primary (Basic) Data Types
+//int
+//float
+//double
+//char
+//void
+
+//Derived Data Types
+//Array
+//Pointer
+//Structure (struct)
+//Union
+//Function
+
+//User-defined Data Types
+//typedef
+//enum
+//struct
+//union
 
 printf("");
 getch();
